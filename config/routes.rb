5482aee7f6552a94c29e8e94c90631c93fb4c7d6
gitpetr <pkhilenko@yandex.ru>
2016-11-16 12:45:49 +0600

    add foto to gitignor

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#landing'
   get 'pages/about' 
+  get '/profiles/:id/subscribe' => 'profiles#subscribe', as: 'subscribe_profile'
+  get '/profiles/:id/unsubscribe' => 'profiles#unsubscribe', as: 'unsubscribe_profile' 
   get '/profiles/:id' => 'profiles#show', as: "profile" 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115173116) do
+ActiveRecord::Schema.define(version: 20161116061902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20161115173116) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_photos_on_user_id", using: :btree
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_subscriptions_on_friend_id", using: :btree
+    t.index ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
