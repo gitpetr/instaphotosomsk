90ef4f46bf391e02ffbaec28e7b2b75218f5f406
gitpetr <pkhilenko@yandex.ru>
2016-11-16 23:05:55 +0600

    add 3 pages in profiles view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#landing'
   get 'pages/about' 
+
+  get 'profiles/my_photos'
+  get 'profiles/subscribes_list'
+  get 'profiles/friends_photos'
+  
   get '/profiles/:id/subscribe' => 'profiles#subscribe', as: 'subscribe_profile'
   get '/profiles/:id/unsubscribe' => 'profiles#unsubscribe', as: 'unsubscribe_profile' 
   get '/profiles/:id' => 'profiles#show', as: "profile" 
