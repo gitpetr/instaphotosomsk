8fd5b63ba744b0fdb84a0d2f482777217cd08ed3
gitpetr <pkhilenko@yandex.ru>
2016-11-16 11:19:33 +0600

    create profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   resources :photos
   devise_for :users
   root 'pages#landing'
   get 'pages/about' 
+  get '/profiles/:id' => 'profiles#show', as: "profile" 
 end
